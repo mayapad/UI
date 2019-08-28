e528d7f075e66cbcf38c69c30aecceae5a0f0a2a
nakashimakazuya <pgmckazu3@yahoo.co.jp>
2019-08-28 11:53:18 +0900

    UI　3-3

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+//            EditTextの文字列をTextViewに設定
+           textView.text = editText.text.toString()
+        }
+
+
+    }
+}
